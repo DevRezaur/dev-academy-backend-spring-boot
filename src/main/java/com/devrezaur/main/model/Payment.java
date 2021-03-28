e2b889827d426661b37bb14cf501f5284142839c
Rezaur Rahman <rezaur.official@gmail.com>
2021-03-29 02:08:56 +0600

    Added: Payment & Post model

---
+++ src/main/java/com/devrezaur/main/model/Payment.java
@@ -0,0 +1,98 @@
+package com.devrezaur.main.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import org.springframework.stereotype.Component;
+
+@Entity
+@Table(name = "payments")
+@Component
+public class Payment {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "pay_id")
+	private int id;
+
+	@Column(name = "user_id")
+	private int userId;
+
+	@Column(name = "course_id")
+	private int courseId;
+
+	@Column(name = "trx_code")
+	private String trxCode;
+
+	@Column(name = "amount")
+	private Double amount;
+
+	@Column(name = "status", columnDefinition = "varchar(15) default 'Pending'")
+	private String status;
+
+	public Payment() {
+		super();
+	}
+
+	public Payment(int id, int userId, int courseId, String trxCode, Double amount, String status) {
+		super();
+		this.id = id;
+		this.userId = userId;
+		this.courseId = courseId;
+		this.trxCode = trxCode;
+		this.amount = amount;
+		this.status = status;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public int getCourseId() {
+		return courseId;
+	}
+
+	public void setCourseId(int courseId) {
+		this.courseId = courseId;
+	}
+
+	public String getTrxCode() {
+		return trxCode;
+	}
+
+	public void setTrxCode(String trxCode) {
+		this.trxCode = trxCode;
+	}
+
+	public Double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+}
---
+++ src/main/java/com/devrezaur/main/model/Post.java
@@ -0,0 +1,98 @@
+package com.devrezaur.main.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import org.springframework.stereotype.Component;
+
+@Entity
+@Table(name = "posts")
+@Component
+public class Post {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "post_id")
+	private int id;
+
+	@Column(name = "course_id")
+	private int courseId;
+
+	@Column(name = "title")
+	private String title;
+
+	@Column(name = "desc")
+	private String desc;
+
+	@Column(name = "resources_link")
+	private String resourcesLink;
+
+	@Column(name = "class_link")
+	private String classLink;
+
+	public Post() {
+		super();
+	}
+
+	public Post(int id, int courseId, String title, String desc, String resourcesLink, String classLink) {
+		super();
+		this.id = id;
+		this.courseId = courseId;
+		this.title = title;
+		this.desc = desc;
+		this.resourcesLink = resourcesLink;
+		this.classLink = classLink;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getCourseId() {
+		return courseId;
+	}
+
+	public void setCourseId(int courseId) {
+		this.courseId = courseId;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDesc() {
+		return desc;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+
+	public String getResourcesLink() {
+		return resourcesLink;
+	}
+
+	public void setResourcesLink(String resourcesLink) {
+		this.resourcesLink = resourcesLink;
+	}
+
+	public String getClassLink() {
+		return classLink;
+	}
+
+	public void setClassLink(String classLink) {
+		this.classLink = classLink;
+	}
+
+}
