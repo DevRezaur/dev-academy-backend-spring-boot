9f837089281958a0282350eda4fc03ebe61b1ac0
Rezaur Rahman <rezaur.official@gmail.com>
2021-04-11 18:35:19 +0600

    Updated: Post Service

--- src/main/java/com/devrezaur/main/controller/AuthController.java
+++ src/main/java/com/devrezaur/main/controller/AuthController.java
@@ -10,7 +10,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.devrezaur.main.model.JwtResponse;
 import com.devrezaur.main.model.Role;
--- src/main/java/com/devrezaur/main/service/PostService.java
+++ src/main/java/com/devrezaur/main/service/PostService.java
@@ -1,5 +1,6 @@
 package com.devrezaur.main.service;
 
+import java.util.Collections;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,7 +14,10 @@ public class PostService {
 	private PostRepository postRepo;
 	
 	public List<Post> getPosts(int courseId) {
-		return postRepo.findByCourseId(courseId);
+		List<Post> posts = postRepo.findByCourseId(courseId);
+		if(posts != null)
+			Collections.reverse(posts);
+		return posts;
 	}
 	
 	public Post createPost(Post post) {
